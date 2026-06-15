69b64eac54c2a58ba3118db343463cba234c3a40
linhnv-1913 <nguyen.van.linh-c@sun-asterisk.com>
2026-06-15 15:30:47 +0700

    add command to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite"; // Chắc chắn là có dòng import này nhé
+
 export default defineConfig(() => {
   return {
-    base: '/tiem-sua-chua/', // ĐỔI THÀNH TÊN REPOSITORY CỦA BẠN
+    base: "/tiem-sua-chua/", // Nhớ giữ dòng base này bằng tên repository của bạn
     plugins: [react(), tailwindcss()],
-    resolve: { /* ... */ },
-    server: { /* ... */ }
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
+      },
+    },
   };
 });
